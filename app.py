ab0089b4fc9aba9ab1cd7a38db36665b2b28559e
tdushime <dushi.the@gmail.com>
2023-11-21 16:44:06 -0500

    openai secret

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
-    openai_key = os.getenv("OPENAI_API_KEY")
+    openai_key = os.getenv("OPENAI_API_KEY") or st.secrets["OPENAI_API_KEY"]
     st.set_page_config(page_title="Chat with PDF docs", page_icon=":robot_face:")
 
     st.write(css, unsafe_allow_html=True)
